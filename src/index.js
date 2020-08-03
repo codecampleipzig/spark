0e629941ff9649e90a38f44e1a4a39004ec7355b
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-08-03 16:21:09 +0200

    Add the capability to list ideas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+const fs = require("fs");
+const os = require("os");
+
+const sparkFilePath = os.homedir() + "/.spark.json";
+
 const command = process.argv[2];
 
 if (command == "create") {
   const idea = process.argv[3];
   console.log("... create a spark: " + idea);
 } else if (command == "list") {
-  console.log("... list all sparks");
+  // Read the .json file
+  const fileContent = fs.readFileSync(sparkFilePath, "utf-8");
+
+  // Convert the json string into an object
+  const data = JSON.parse(fileContent);
+
+  // console.log the sparks as a table
+  console.table(data.ideas);
 } else if (command == "extinguish") {
   const index = process.argv[3];
   console.log("... remove spark with index " + index);
