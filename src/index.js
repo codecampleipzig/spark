69d18d5cc848e24d9b9afe3c0b070c5a51d2fcab
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-08-03 15:33:49 +0200

    Add parsing for command line arguments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const command = process.argv[2];
+
+if (command == "create") {
+  const idea = process.argv[3];
+  console.log("... create a spark: " + idea);
+} else if (command == "list") {
+  console.log("... list all sparks");
+} else if (command == "extinguish") {
+  const index = process.argv[3];
+  console.log("... remove spark with index " + index);
+} else {
+  console.log("Print the help");
+}
