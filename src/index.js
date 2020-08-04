977eeffed38d74e6f38b5fff55887da66fc5f75e
Gabriel Heinrich <gabriel@codecampleipzig.de>
2020-08-04 11:54:43 +0200

    Refactor the index.js file into a module

--- src/index.js
+++ src/index.js
@@ -1,55 +1,14 @@
 #! /usr/bin/env node
-const fs = require("fs");
-const os = require("os");
-
-const sparkFilePath = os.homedir() + "/.spark.json";
-const loadSparkFile = function () {
-  try {
-    // Read the .json file
-    const fileContent = fs.readFileSync(sparkFilePath, "utf-8");
-
-    // Convert the json string into an object and return it
-    return JSON.parse(fileContent);
-  } catch (error) {
-    return {
-      ideas: [],
-    };
-  }
-};
+const spark = require("./spark.js");
 
 const command = process.argv[2];
 
 if (command == "create") {
-  // Read the sparks from the file
-  const data = loadSparkFile();
-
-  // Add our new spark to the ideas array
-  data.ideas.push({
-    idea: process.argv[3],
-  });
-
-  // Convert the data object into a JSON string
-  const fileContent = JSON.stringify(data);
-  // Write the JSON string again into the file
-  fs.writeFileSync(sparkFilePath, fileContent);
+  spark.create(process.argv[3]);
 } else if (command == "list") {
-  const data = loadSparkFile();
-
-  // console.log the sparks as a table
-  console.table(data.ideas);
+  spark.list();
 } else if (command == "extinguish" || command == "remove") {
-  const index = Number(process.argv[3]);
-
-  // Read the sparks from the file
-  const data = loadSparkFile();
-
-  // Add our new spark to the ideas array
-  data.ideas.splice(index, 1);
-
-  // Convert the data object into a JSON string
-  const fileContent = JSON.stringify(data);
-  // Write the JSON string again into the file
-  fs.writeFileSync(sparkFilePath, fileContent);
+  spark.remove(Number(process.argv[3]));
 } else {
   console.log("Print the help");
 }
---
+++ src/spark.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const os = require("os");
+
+const sparkFilePath = os.homedir() + "/.spark.json";
+
+const loadSparkFile = function () {
+  try {
+    const fileContent = fs.readFileSync(sparkFilePath, "utf-8");
+
+    return JSON.parse(fileContent);
+  } catch (error) {
+    return {
+      ideas: [],
+    };
+  }
+};
+
+const storeSparkFile = function (data) {
+  // Convert the data object into a JSON string
+  const fileContent = JSON.stringify(data);
+  // Write the JSON string again into the file
+  fs.writeFileSync(sparkFilePath, fileContent);
+
+  listSparks();
+};
+
+const createSpark = function (idea) {
+  const data = loadSparkFile();
+
+  // Add our new spark to the ideas array
+  data.ideas.push({
+    idea: idea,
+  });
+
+  storeSparkFile(data);
+};
+
+const listSparks = function () {
+  const data = loadSparkFile();
+
+  // console.log the sparks as a table
+  console.table(data.ideas);
+};
+
+const removeSpark = function (index) {
+  const data = loadSparkFile();
+
+  // Add our new spark to the ideas array
+  data.ideas.splice(index, 1);
+
+  storeSparkFile(data);
+  listSparks();
+};
+
+module.exports = {
+  create: createSpark,
+  list: listSparks,
+  remove: removeSpark,
+};
